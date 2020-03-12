b75c7819a98a6c7fb2722d1a68f57d5b718a753b
silver <silverabel@users.noreply.github.com>
2020-03-12 16:29:25 +0000

    taimer

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -3,6 +3,7 @@ var kastKlassigaElemendid = document.getElementsByClassName("kast");
 var muudaMitmeSisestatudNumbriFonti = function() {
   this.style.fontSize = "0.8em";
   this.style.paddingBottom = "24px";
+  this.style.paddingLeft = "1px";
   this.style.textAlign = "left";
 };
 
@@ -10,11 +11,13 @@ var muudaMitmeSisestatudNumbriFonti2 = function() {
   if (this.value.length > 1) {
     this.style.fontSize = "0.8em";
     this.style.paddingBottom = "0px";
+    this.style.paddingLeft = "0px";
     this.style.textAlign = "center";
   }
   if (this.value.length == 1) {
     this.style.fontSize = "2em";
     this.style.paddingBottom = "0px";
+    this.style.paddingLeft = "0px";
     this.style.textAlign = "center";
   }
 };
@@ -31,6 +34,7 @@ var vahetaChecki = function(key) {
     if (document.getElementById("teine").checked == true) {
       document.getElementById("esimene").checked = true;
       fookusesKast.style.paddingBottom = "0px";
+      fookusesKast.style.paddingLeft = "1px";
       fookusesKast.style.textAlign = "center";
       if (fookusesKast.value.length == 1) {
         fookusesKast.style.fontSize = "2em";
@@ -43,6 +47,7 @@ var vahetaChecki = function(key) {
     else {
       document.getElementById("teine").checked = true;
         fookusesKast.style.paddingBottom = "24px";
+        fookusesKast.style.paddingLeft = "1px";
         fookusesKast.style.textAlign = "left";
         fookusesKast.style.fontSize = "0.8em";
       for (var i = 0; i < kastKlassigaElemendid.length; i++) {
@@ -61,3 +66,26 @@ var teeFookusesKastTühjaks = function(key) {
 };
 
 document.addEventListener('keydown', teeFookusesKastTühjaks);
+
+
+// ajanäit
+var minutid = 0;
+var sekundid = 0;
+
+function muudaAega() {
+  if (sekundid == 60) {
+    minutid += 1;
+    sekundid = 0;
+    document.getElementById("minutid").innerHTML = (minutid ? (minutid > 9 ? minutid : "0" + minutid) : "00");
+    document.getElementById("sekundid").innerHTML = 0;
+  }
+  else {
+    document.getElementById("sekundid").innerHTML = (sekundid ? (sekundid > 9 ? sekundid : "0" + sekundid) : "00");
+  }
+  sekundid += 1;
+  setTimeout(muudaAega, 1000);
+}
+
+muudaAega();
+
+
